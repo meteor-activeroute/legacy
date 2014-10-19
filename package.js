53de77051ccea79bbc9fed2330aed937143b51f8
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2014-10-19 23:21:02 +0200

    Change version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/zimme/meteor-iron-router-active.git',
   name: 'zimme:iron-router-active',
   summary: 'Active route/path template helpers for iron:router',
-  version: '1.0.0-pre.2'
+  version: '0.3.0-pre.1'
 });
 
 Package.onUse(function(api) {
