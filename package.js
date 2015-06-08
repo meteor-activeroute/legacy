d5d73cadeb7cea758491fb1e06d3da790f273e75
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2015-06-08 21:55:25 +0200

    Version bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/zimme/meteor-active-route.git',
   name: 'zimme:active-route',
   summary: 'Active route helpers',
-  version: '2.0.4_1'
+  version: '2.0.5-rc.1'
 });
 
 Package.onUse(function(api) {
